3f91c37ccfd8c1875bb8cdb3232a14ef99313a76
eight <eight04@gmail.com>
2017-11-22 07:56:18 +0800

    Fix: improve checkbox-supplement, add prefUpdate event

--- options/options.js
+++ options/options.js
@@ -5,4 +5,23 @@
 	var inputs = form.querySelectorAll("input");
 	
 	pref.bindElement(form, inputs);
+	
+	pref.ready().then(() => {
+		// checkbox supplement
+		const fields = document.querySelectorAll("fieldset.checkbox-supplement");
+		for (const field of fields) {
+			const input = field.parentNode.querySelector(":scope > input");
+			if (!input.checked) {
+				field.disabled = true;
+			}
+			input.addEventListener("prefUpdate", onChange);
+		}
+		
+		function onChange(e) {
+			const input = e.target;
+			const field = input.parentNode.querySelector(":scope > fieldset");
+			if (!field) return;
+			field.disabled = !input.checked;
+		}
+	});
 })();
