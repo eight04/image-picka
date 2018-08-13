64cca44e0c567195681b2d02a5719e87892311b9
eight <eight04@gmail.com>
2018-08-13 17:48:40 +0800

    Revert: pageTitle fallback

--- extension/background/background.js
+++ extension/background/background.js
@@ -695,11 +695,6 @@ function expandEnv(env) {
 	// page url
 	url = new URL(env.pageUrl);
 	env.pageHostname = url.hostname;
-	
-	// page title fallback
-	if (!env.pageTitle) {
-		env.pageTitle = env.pageUrl;
-	}
 }
 
 function nestDecodeURIComponent(s) {
