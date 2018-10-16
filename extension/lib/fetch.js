150c90000e141102c2bb177579eb5f5f757d172c
eight <eight04@gmail.com>
2018-10-16 19:23:25 +0800

    Add: support cache in Chrome

--- extension/lib/fetch.js
+++ extension/lib/fetch.js
@@ -1,21 +1,34 @@
 /* exported fetchBlob */
 
 function fetchBlob(url) {
-	// seems that we can hit the cache with XMLHttpRequest
-	// https://github.com/eight04/image-picka/pull/23
-	return new Promise((resolve, reject) => {
-		const r = new XMLHttpRequest;
-		r.open("GET", url);
-		r.responseType = "blob";
-		r.onload = () => {
-			resolve(r.response);
-		};
-		r.onerror = () => {
-			reject(new Error(`Failed to load: ${url}`));
-		};
-		r.ontimeout = () => {
-			reject(new Error(`Connection timeout: ${url}`));
-		};
-		r.send();
-	});
+	if (/Firefox\/\d+/.test(navigator.userAgent)) {
+		return fetchBlobFirefox();
+	}
+	return fetchBlobChrome();
+	
+	function fetchBlobFirefox() {
+		// seems that we can hit the cache with XMLHttpRequest
+		// https://github.com/eight04/image-picka/pull/23
+		return new Promise((resolve, reject) => {
+			const r = new XMLHttpRequest;
+			r.open("GET", url);
+			r.responseType = "blob";
+			r.onload = () => {
+				resolve(r.response);
+			};
+			r.onerror = () => {
+				reject(new Error(`Failed to load: ${url}`));
+			};
+			r.ontimeout = () => {
+				reject(new Error(`Connection timeout: ${url}`));
+			};
+			r.send();
+		});
+	}
+	
+	function fetchBlobChrome() {
+		// in Chrome we use no-cors + force-cache
+		return fetch(url, {mode: "no-cors", cache: "force-cache"})
+			.then(r => r.blob());
+	}
 }
