c8ab3b82f4b072fe5b8936283f2c442a5f1f7a3d
eight <eight04@gmail.com>
2018-10-18 20:26:59 +0800

    Add: support srcset from img/source

--- extension/content/dragndrop.js
+++ extension/content/dragndrop.js
@@ -17,6 +17,32 @@ const getImageSrc = (() => {
 			.map(p => p.trim())
 			.filter(Boolean);
 	}
+	
+	function getSrcFromSrcset(set) {
+		const rules = set.split(/\s*,\s*/).map(rule =>
+			rule.split(/\s+/).reduce((result, token) => {
+				if (token) {
+					let match;
+					if ((match = token.match(/^(\d+)[wx]$/))) {
+						result.scale = +match[1];
+					} else {
+						result.url = token;
+					}
+				}
+				return result;
+			}, {
+				scale: 1
+			})
+		);
+		
+		let maxRule;
+		for (const rule of rules) {
+			if (!maxRule || rule.scale > maxRule.scale) {
+				maxRule = rule;
+			}
+		}
+		return maxRule.url;
+	}
 		
 	return img => {
 		for (const prop of SRC_PROP) {
@@ -25,7 +51,22 @@ const getImageSrc = (() => {
 				return new URL(src, location.href).href;
 			}
 		}
-		return img.src;
+		if (img.src) {
+			return img.src;
+		}
+		let srcset;
+		if (img.srcset) {
+			srcset = img.srcset;
+		} else if (
+			img.previousElementSibling && 
+			img.previousElementSibling.nodeName === "SOURCE" &&
+			img.previousElementSibling.srcset
+		) {
+			srcset = img.previousElementSibling.srcset;
+		}
+		if (srcset) {
+			return getSrcFromSrcset(srcset);
+		}
 	};
 })();
 
