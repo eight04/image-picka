f7034310cc0277832adaab836dccfb1ff2168f74
eight <eight04@gmail.com>
2019-01-03 20:20:30 +0800

    Add: handle referrerPolicy in content.js

--- extension/content/content.js
+++ extension/content/content.js
@@ -1,6 +1,9 @@
 /* global initDownloadSingleImage getImageSrc pref fetchImage urlMap */
 
 (() => {
+  let referrerMeta;
+  const IS_FIREFOX = typeof InstallTrigger !== "undefined";
+  
 	browser.runtime.onMessage.addListener(message => {
 		switch (message.method) {
 			case "getEnv":
@@ -16,14 +19,10 @@
 	
 	initDownloadSingleImage({downloadImage});
 	
-	function isFirefox() {
-		return typeof InstallTrigger !== "undefined";
-	}
-
-	function downloadImage(url) {
+	function downloadImage(url, referrerPolicy = getDefaultReferrerPolicy()) {
 		url = urlMap.transform(url);
 		let image;
-		(pref.get("useCache") ? fetchImageData(url) : Promise.resolve({url}))
+		(pref.get("useCache") ? fetchImageData(url, referrerPolicy) : Promise.resolve({url}))
 			.then(_image => {
 				image = _image;
 				return browser.runtime.sendMessage({
@@ -53,36 +52,54 @@
 			});
 	}
   
-  function fetchImageData(url) {
-    return fetchImage(url)
-      .catch(err => {
-        if (!isFirefox()) {
-          // https://github.com/eight04/image-picka/issues/158
-          return browser.runtime.sendMessage({method: "fetchImage", url})
-            .then(data => {
-              data.fromBackground = true;
-              return data;
-            });
-        }
-        throw err;
-      })
-      .then(data => {
-        if (!isFirefox() && data.blob) {
-          data.blobUrl = URL.createObjectURL(data.blob);
-          delete data.blob;
-        }
-        return data;
-      });
+  function fetchImageData(url, referrerPolicy) {
+    return doFetch().then(data => {
+      if (!IS_FIREFOX && data.blob) {
+        data.blobUrl = URL.createObjectURL(data.blob);
+        delete data.blob;
+      }
+      return data;
+    });
+    
+    function doFetch() {
+      // use background fetch in Chrome
+      if (!IS_FIREFOX && getReferrer(location.href, url, referrerPolicy) != location.href) {
+        return fetchImageFromBackground();
+      }
+      return fetchImage(url)
+        .catch(err => {
+          if (!IS_FIREFOX) {
+            // fallback to background fetch if mixed content error occurred
+            // https://github.com/eight04/image-picka/issues/158
+            return fetchImageFromBackground();
+          }
+          throw err;
+        });
+    }
+      
+    function fetchImageFromBackground() {
+      return browser.runtime.sendMessage({method: "fetchImage", url})
+        .then(data => {
+          data.fromBackground = true;
+          return data;
+        });
+    }
   }
 	
 	function getImages() {
-		let images = [...document.images]
-			.map(getImageSrc)
-			.filter(Boolean)
-			.filter(u => !u.startsWith("moz-extension://"))
-			.map(urlMap.transform);
-		images = [...new Set(images)];
-		return images;
+    const images = new Map;
+    for (const img of document.images) {
+      const src = getImageSrc(img);
+      if (!src || /^[\w]+-extension/.test(src)) {
+        continue;
+      }
+      const url = urlMap.transform(src);
+      images.set(url, {
+        url,
+        noReferrer: getReferrer(location.href, url, img.referrerPolicy || getDefaultReferrerPolicy()) !== location.href
+      });
+    }
+		return [...images.values()];
 	}
 	
 	function getEnv() {
@@ -90,5 +107,25 @@
 			pageTitle: document.title,
 			pageUrl: location.href
 		};
-	}	
+	}
+  
+  function getDefaultReferrerPolicy() {
+    if (referrerMeta === undefined) {
+      referrerMeta = document.querySelector('meta[name="referrer"]') || null;
+    }
+    return referrerMeta && referrerMeta.content || "";
+  }
+  
+  function getReferrer(documentUrl, target, policy) {
+    if (policy === "no-referrer") {
+      return "";
+    }
+    if (policy === "no-referrer-when-downgrade") {
+      return documentUrl.startsWith("https:") && target.startsWith("http:") ?
+        "" : documentUrl;
+    }
+    // FIXME: add more cases
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+    return documentUrl;
+  }
 })();
