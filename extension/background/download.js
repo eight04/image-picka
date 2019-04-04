82e78bdf05a9fa9e7e510379c63fe859059eeeb4
eight <eight04@gmail.com>
2019-04-04 18:20:44 +0800

    Refactor: throttle download api

--- extension/background/download.js
+++ extension/background/download.js
@@ -1,75 +1,83 @@
+/* global throttle defer */
 /* exported download */
 
-function download(options, wait = false) {
-	const {resolve, reject, promise} = deferred();
-	browser.downloads.onChanged.addListener(handleChange);
-	let blobUrl;
-	const getDownloadId = tryFetchBlob().then(() => browser.downloads.download(options));
-	getDownloadId.catch(err => {
-		reject(err);
-		cleanup();
-	});
-	if (wait) {
-		return promise;
-	}
-	return getDownloadId;
-	
-	function tryFetchBlob() {
-		const blob = options.blob;
-		delete options.blob;
-		let doFetch;
-		// download API in Firefox can't handle cross origin blobs and data urls.
-		if (/^data:/.test(options.url)) {
-			doFetch = fetch(options.url).then(r => r.blob());
-		} else if (blob) {
-			doFetch = Promise.resolve(blob);
-		} else {
-			return Promise.resolve();
-		}
-		return doFetch.then(blob => {
-			if (!blob.size) {
-				throw new Error(`Failed to fetch: ${options.url}`);
-			}
-			blobUrl = URL.createObjectURL(blob);
-			options.url = blobUrl;
-		});
-	}
-	
-	function handleChange(changes) {
-		if (!changes.state) {
+const download = (() => {
+  const tasks = new Map;
+  const que = throttle();
+  browser.downloads.onChanged.addListener(handleChange);
+  return download;
+  
+  function handleChange(changes) {
+    // FIXME: is it possible that the task state changes before the id is set?
+		if (!changes.state || !tasks.has(changes.id)) {
 			return;
 		}
 		const state = changes.state.current;
 		if (state === "interrupted") {
-			getDownloadId.then(id => {
-				if (id === changes.id) {
-					reject(changes.error);
-					cleanup();
-				}
-			});
+      tasks.get(changes.id).q.reject(changes.error);
 		} else if (state === "complete") {
-			getDownloadId.then(id => {
-				if (id === changes.id) {
-					resolve();
-					cleanup();
-				}
-			});
+      tasks.get(changes.id).q.resolve();
 		}
-	}
-	
-	function cleanup() {
-		browser.downloads.onChanged.removeListener(handleChange);
-		if (blobUrl) {
-			URL.revokeObjectURL(blobUrl);
-		}
-	}
-	
-	function deferred() {
-		const o = {};
-		o.promise = new Promise((resolve, reject) => {
-			o.resolve = resolve;
-			o.reject = reject;
-		});
-		return o;
-	}
-}
+  }
+  
+  function download(options, wait = false) {
+    const task = {
+      qStart: defer(),
+      q: defer()
+    };
+    que.addCallback(done => {
+      // always delete blob property
+      const blob = options.blob;
+      delete options.blob;
+      if (/^data:/.test(options.url)) {
+        // download API in Firefox can't handle cross origin blobs and data urls.
+        fetch(options.url)
+          .then(r => r.blob())
+          .then(downloadBlob)
+          .then(task.qStart.resolve, task.qStart.reject);
+      } else if (blob) {
+        downloadBlob(blob)
+          .then(task.qStart.resolve, task.qStart.reject);
+      } else {
+        doDownload()
+          .then(task.qStart.resolve, task.qStart.reject);
+      }
+      task.q.promise
+        .catch(() => {})
+        .then(cleanup);
+        
+      function downloadBlob(blob) {
+        task.blobUrl = URL.createObjectURL(blob);
+        options.url = task.blobUrl;
+        return doDownload();
+      }
+      
+      function doDownload() {
+        return browser.downloads.download(options)
+          .then(id => {
+            task.id = id;
+            tasks.set(id, task);
+          })
+          .catch(err => {
+            cleanup();
+            throw err;
+          });
+      }
+      
+      function cleanup() {
+        if (task.blobUrl) {
+          URL.revokeObjectURL(task.blobUrl);
+          task.blobUrl = null;
+        }
+        if (task.id) {
+          tasks.delete(task);
+        }
+        done();
+      }
+    });
+    if (wait) {
+      return task.q.promise;
+    }
+    return task.qStart.promise;
+  }
+})();
