77fb3b9e603ade8599092a6c539ee622d9a9b826
eight <eight04@gmail.com>
2019-10-28 19:13:07 +0800

    Add: responsive picker

--- extension/picker/picker.js
+++ extension/picker/picker.js
@@ -8,6 +8,14 @@ browser.runtime.sendMessage({method: "getBatchData", batchId: BATCH_ID})
 			.then(domReady)
 			.then(() => init(req))
 	);
+  
+// toolbar expand
+for (const el of document.querySelectorAll(".toolbar")) {
+  el.querySelector(".toolbar-expand-button").addEventListener("click", () => {
+    console.log("toggle");
+    el.classList.toggle("expanded");
+  });
+}
 	
 function domReady() {
 	if (document.readyState !== "loading") {
