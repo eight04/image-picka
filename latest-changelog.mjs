164d7ecae3e6526f14e1b78fadca0664ce35a427
eight04 <eight04@gmail.com>
2025-10-17 22:32:15 +0800

    Bump deps

---
+++ latest-changelog.mjs
@@ -0,0 +1,25 @@
+// print lagest changelog entry from README.md
+
+import fs from 'fs';
+
+const readmePath = './README.md';
+const readmeContent = fs.readFileSync(readmePath, 'utf-8');
+
+const rxTitle = /^Changelog\r?\n[-=]+\r?\n/gm;
+const rxEntry = /^\*[^\r\n]+/gm;
+
+const titleMatch = rxTitle.exec(readmeContent);
+if (!titleMatch) {
+  throw new Error('Changelog title not found in README.md');
+}
+
+rxEntry.lastIndex = rxTitle.lastIndex;
+const firstEntryMatch = rxEntry.exec(readmeContent);
+const lastEntryMatch = rxEntry.exec(readmeContent);
+
+if (!firstEntryMatch) {
+  throw new Error('No changelog entries found in README.md');
+}
+const startIndex = firstEntryMatch.index;
+const endIndex = lastEntryMatch ? lastEntryMatch.index : readmeContent.length;
+console.log(readmeContent.slice(startIndex, endIndex).trim());
